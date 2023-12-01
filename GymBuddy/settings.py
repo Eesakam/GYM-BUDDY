365da3332152edcc59993de85c2552d45f78969a
Eesakam <Ekamaldien56@gmail.com>
2023-12-01 19:40:35 +0200

    Second commit

--- GymBuddy/settings.py
+++ GymBuddy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*p$wj6ygee%o!kdj+qr4zk5*c36kgu0=#6c!8hw5emlec8$b&)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
